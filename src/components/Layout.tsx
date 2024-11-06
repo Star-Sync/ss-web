3ba4fc08e6452f22a09e29e00f8548dcb0babb33
Sathira Williams <sathira.williams@gmail.com>
2024-11-05 22:31:41 -0500

    fix: Fixed ESLint empty interface & added /.idea/ to .gitignore

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,22 @@
-import React, { ReactNode} from 'react';
-import {SidebarProvider} from "@/components/ui/sidebar";
-import {AppSidebar} from "@/components/AppSidebar";
-import Header from "@/components/Header";
+import React from 'react';
+import { useRouter } from 'next/router';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/AppSidebar';
+import Header from '@/components/Header';
 
 type LayoutProps = {
-    children: ReactNode;
+    children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+    const router = useRouter();
+    const isHomepage = router.pathname === '/';
+
     return (
         <SidebarProvider>
-            <AppSidebar />
+            {!isHomepage && <AppSidebar />}
             <main className="w-screen h-screen">
-                <Header/>
+                {!isHomepage && <Header />}
                 {children}
             </main>
         </SidebarProvider>
