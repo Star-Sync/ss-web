eb4e8d2ff7dafc6ef53c25760fc5d5e2c234ed22
Slava <1219slava@gmail.com>
2024-11-07 18:58:06 -0500

    UI homepage in tsx now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import './App.css';
+import './index.css';
 import '../styles/globals.css';
+import '../components/article/article.css';  // Add this import here for global styles like article.css
+import '../components/brand/brand.css';
+import '../components/cta/cta.css';
+import '../components/feature/feature.css';
+import '../components/navbar/navbar.css';
+
+import '../containers/blog/blog.css';
+import '../containers/features/features.css';
+import '../containers/footer/footer.css';
+import '../containers/header/header.css';
+import '../containers/possibility/possibility.css';
+import '../containers/whatScheduler/whatScheduler.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Footer, Blog, Possibility, Features, WhatScheduler, Header } from '../containers';
+import { Brand, CTA, Navbar } from '../components';
+
 
 const Home: React.FC = () => {
-    return (
-        <section className="w-full h-full flex flex-col bg-gray-700">
-            <div>
-                <h1 className="text-2xl font-bold mb-4 text-white text-center mt-72">This is intentionally blank</h1>
-            </div>
-        </section>
-
-    );
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatScheduler />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
