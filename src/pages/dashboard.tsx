05717378b8a129aaf96a062998a07c74cf4bf62c
Sathira <sathira.williams@gmail.com>
2024-11-08 20:59:43 -0500

    feat: dashboard-upcoming component added

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import DashboardUpcoming from "@/components/app/dashboard/dashboard-upcoming";
 
 const Dashboard: React.FC = () => {
     const [isLoaded, setIsLoaded] = useState(false);
@@ -27,9 +28,9 @@ const Dashboard: React.FC = () => {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                     <AnimatePresence>
                         {[
-                            { color: 'bg-blue-500', title: 'Scheduled Tasks', text: 'Number of tasks scheduled today: 10' },
-                            { color: 'bg-green-500', title: 'Active Satellites', text: 'Satellites in operation: 5' },
-                            { color: 'bg-yellow-500', title: 'Pending Requests', text: 'Number of pending requests: 3' }
+                            { color: "bg-blue-500", title: "Scheduled Tasks", text: "Number of tasks scheduled today: 10" },
+                            { color: "bg-green-500", title: "Active Satellites", text: "Satellites in operation: 5" },
+                            { color: "bg-yellow-500", title: "Pending Requests", text: "Number of pending requests: 3" },
                         ].map((card, index) => (
                             <motion.div
                                 key={card.title}
@@ -44,6 +45,22 @@ const Dashboard: React.FC = () => {
                         ))}
                     </AnimatePresence>
                 </div>
+            </motion.div>
+
+            {/* Add DashboardUpcoming Component */}
+            <motion.div
+                className="bg-white my-2"
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                transition={{duration: 0.5}}
+            >
+                <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                    <h2 className="text-xl font-bold text-gray-900 grid-cols-1 grid-rows-1">Upcoming Missions</h2>
+                    <DashboardUpcoming className="row-start-2"/>
+                </div>
+
+                {/*<h2 className="text-xl font-bold text-gray-900 p-3">Upcoming Missions</h2>*/}
+
             </motion.div>
         </motion.section>
     );
