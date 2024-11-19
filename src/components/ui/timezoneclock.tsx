b00c0cff09094f31c66e08c850e37933bcf9f667
Sathira Williams <sathira.williams@gmail.com>
2024-11-19 16:21:29 -0500

    feat: Added Date to timezoneclock.tsx

--- src/components/ui/timezoneclock.tsx
+++ src/components/ui/timezoneclock.tsx
@@ -19,6 +19,7 @@ interface TimezoneClockProps {
 
 const TimezoneClock: React.FC<TimezoneClockProps> = ({ open, setOpen }) => {
     const [time, setTime] = useState<string>("");
+    const [date, setDate] = useState<string>("");
     const [timezone, setTimezone] = useState<string>("UTC");
 
     useEffect(() => {
@@ -32,16 +33,31 @@ const TimezoneClock: React.FC<TimezoneClockProps> = ({ open, setOpen }) => {
     useEffect(() => {
         const timer = setInterval(() => {
             const now = new Date();
-            const formatter = new Intl.DateTimeFormat("en-US", {
+
+            // Format time
+            const timeFormatter = new Intl.DateTimeFormat("en-US", {
                 timeZone: timezone,
                 hour: "2-digit",
                 minute: "2-digit",
                 second: "2-digit",
                 hour12: false,
             });
-            const formattedTime = formatter.format(now);
+            const formattedTime = timeFormatter.format(now);
+
+            // Format date
+            const dateFormatter = new Intl.DateTimeFormat("en-US", {
+                timeZone: timezone,
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+            });
+            const formattedDate = dateFormatter.format(now);
+
+            // Set time and date
             const locationName = timezones.find((tz) => tz.value === timezone)?.name || "";
             setTime(`${formattedTime} - ${locationName}`);
+            setDate(formattedDate);
         }, 1000);
 
         return () => clearInterval(timer);
@@ -65,7 +81,10 @@ const TimezoneClock: React.FC<TimezoneClockProps> = ({ open, setOpen }) => {
                     className="w-[250px] justify-between"
                     style={{ minWidth: '250px' }}
                 >
-                    {time}
+                    <div className="flex flex-col text-left">
+                        <span>{time}</span>
+                        <span className="text-xs text-gray-500">{date}</span>
+                    </div>
                     <ChevronsUpDown className="ml-2 opacity-50" />
                 </Button>
             </PopoverTrigger>
