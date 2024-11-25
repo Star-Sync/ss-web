85a6bb2d3c332cfe0024c5d5e471369a4dbe303b
Sathira Williams <45524280+pwSathira@users.noreply.github.com>
2024-11-24 21:03:20 -0500

    [SS-76] Review of Calendar Scheduling (#19)
    * feat: Created .env to handle API
    
    * fix: Removed unnecessary packages
    
    * fix: .env builds upon itself and .production uses SS-CORE's env variables

--- src/pages/app/calendar.tsx
+++ src/pages/app/calendar.tsx
@@ -62,7 +62,7 @@ const CalendarPage = () => {
     const fetchMissions = async () => {
       try {
         const axiosInstance = axios.create({
-          baseURL: "http://localhost:5000",
+          baseURL: process.env.NEXT_PUBLIC_API_URL,
           headers: { "Content-Type": "application/json" },
         });
 
