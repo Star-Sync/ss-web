36e0e096b0322ee271d2819820bd1d5de05b562d
Sathira Williams <sathira.williams@gmail.com>
2025-03-31 17:14:12 -0400

    refactor: Update GroundContainer to use new Tab and TabContent components, remove deprecated TabNav and TabContent files

--- src/components/app/ground/HeaderSection.tsx
+++ src/components/app/ground/HeaderSection.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const HeaderSection: FC = ({}) => {
     return (
-        <div className="flex justify-between items-center mb-4">
+        <div className="flex justify-between items-center mb-4 w-screen">
             <div>
                 <h1 className="text-xl font-semibold text-black">Ground Station Management</h1>
                 <div className="flex items-center text-sm text-gray-500 mt-2">
