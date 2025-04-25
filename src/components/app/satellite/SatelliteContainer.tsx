7c1de6dabb8d75626a547d040c94bddf6d81d606
david <david.zinkiv@gmail.com>
2025-04-25 14:46:21 -0400

    cleaned up the import

--- src/components/app/satellite/SatelliteContainer.tsx
+++ src/components/app/satellite/SatelliteContainer.tsx
@@ -1,61 +1,60 @@
 // SatelliteContainer.tsx
 import React, { FC, useState, useRef } from "react";
-import { Tab } from "@/components/ui/tab/TabNav";
+import TabNav, { Tab } from "@/components/ui/tab/TabNav";
 import MotionWrapper from "../MotionWrapper";
-import TabNav from "@/components/ui/tab/TabNav";
 import HeaderSection from "@/components/app/satellite/HeaderSection";
 import FormManager from "./Form/FormManager";
 import TabContent from "@/components/ui/tab/TabContent";
 import SatelliteGeneral from "@/components/app/satellite/SatelliteGeneral";
 
 const SatelliteContainer: FC = () => {
-    const [tabs, setTabs] = useState<Tab[]>([
-        {
-            id: "overview",
-            name: "Overview",
-            content: () => <SatelliteGeneral />,
-            isPinned: true,
-        },
-    ]);
-    const [activeTabId, setActiveTabId] = useState("overview");
-    const tabCounter = useRef(0);
+  const [tabs, setTabs] = useState<Tab[]>([
+    {
+      id: "overview",
+      name: "Overview",
+      content: () => <SatelliteGeneral />,
+      isPinned: true,
+    },
+  ]);
+  const [activeTabId, setActiveTabId] = useState("overview");
+  const tabCounter = useRef(0);
 
-    const addNewTab = () => {
-        tabCounter.current += 1;
-        const newTabId = `satellite-form-${tabCounter.current}`;
-        const newTab: Tab = {
-            id: newTabId,
-            name: `Form ${tabCounter.current}`,
-            content: () => <FormManager closeTab={closeTab} tabId={newTabId} />,
-        };
-        setTabs((prevTabs) => [...prevTabs, newTab]);
-        setActiveTabId(newTabId);
+  const addNewTab = () => {
+    tabCounter.current += 1;
+    const newTabId = `satellite-form-${tabCounter.current}`;
+    const newTab: Tab = {
+      id: newTabId,
+      name: `Form ${tabCounter.current}`,
+      content: () => <FormManager closeTab={closeTab} tabId={newTabId} />,
     };
+    setTabs((prevTabs) => [...prevTabs, newTab]);
+    setActiveTabId(newTabId);
+  };
 
-    const closeTab = (tabId: string) => {
-        const tabToClose = tabs.find((tab) => tab.id === tabId);
-        if (tabToClose?.isPinned) return;
+  const closeTab = (tabId: string) => {
+    const tabToClose = tabs.find((tab) => tab.id === tabId);
+    if (tabToClose?.isPinned) return;
 
-        setActiveTabId(tabs[0].id);
-        const updatedTabs = tabs.filter((tab) => tab.id !== tabId);
-        setTabs(updatedTabs);
-    };
+    setActiveTabId(tabs[0].id);
+    const updatedTabs = tabs.filter((tab) => tab.id !== tabId);
+    setTabs(updatedTabs);
+  };
 
-    return (
-        <MotionWrapper className="flex flex-col p-6">
-            <div className="rounded-xl p-6 shadow-lg bg-white">
-                <HeaderSection />
-                <TabNav
-                    tabs={tabs}
-                    activeTabId={activeTabId}
-                    setActiveTabId={setActiveTabId}
-                    addNewTab={addNewTab}
-                    closeTab={closeTab}
-                />
-                <TabContent tabs={tabs} activeTabId={activeTabId} />
-            </div>
-        </MotionWrapper>
-    );
+  return (
+    <MotionWrapper className="flex flex-col p-6">
+      <div className="rounded-xl p-6 shadow-lg bg-white">
+        <HeaderSection />
+        <TabNav
+          tabs={tabs}
+          activeTabId={activeTabId}
+          setActiveTabId={setActiveTabId}
+          addNewTab={addNewTab}
+          closeTab={closeTab}
+        />
+        <TabContent tabs={tabs} activeTabId={activeTabId} />
+      </div>
+    </MotionWrapper>
+  );
 };
 
 export default SatelliteContainer;
